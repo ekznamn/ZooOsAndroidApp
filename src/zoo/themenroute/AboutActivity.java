9432299f53470d75ca92b19fd209de67553b4eca
unknown <Nina@.(none)>
2012-09-30 21:08:31 +0200

    Added links to about activity

--- src/zoo/themenroute/AboutActivity.java
+++ src/zoo/themenroute/AboutActivity.java
@@ -3,7 +3,9 @@ package zoo.themenroute;
 
 import android.app.Activity;
 import android.os.Bundle;
-import zoo.themenroute.R;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 /**
  * The About-Activity provides the current App-Version and information about 
@@ -24,5 +26,25 @@ public class AboutActivity extends Activity {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
+		
+		TextView tv_igf = (TextView) findViewById(R.id.about_igfurl);
+		tv_igf.setText(Html.fromHtml(
+				"<a href=\"http://www.igf.uni-osnabrueck.de/index.php/de/\">" + 
+				"www.igf.uni-osnabrueck.de</a>"));
+		tv_igf.setMovementMethod(LinkMovementMethod.getInstance());
+		
+		TextView tv_osm = (TextView) findViewById(R.id.about_osm);
+		tv_osm.setText(Html.fromHtml(
+				"Karte &#169; <a href=\"http://www.openstreetmap.org\">" +
+				"OpenStreetMap</a> - " +
+				"<a href=\"http://www.openstreetmap.org/copyright\">" +
+				"Terms</a>"));
+		tv_osm.setMovementMethod(LinkMovementMethod.getInstance());
+		
+		TextView tv_zoowiso = (TextView) findViewById(R.id.about_zoowiso);
+		tv_zoowiso.setText(Html.fromHtml(
+				getResources().getText(R.string.about_zoowiso) +
+				" <a href=\"http://www.zoowiso-os.de\">ZooWisO</a>."));
+		tv_zoowiso.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 }
